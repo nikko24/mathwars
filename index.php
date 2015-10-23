f870f5c6f633779641b7c353f1a2ab87722b811c
nikko24 <stepan.nikolic@hotmail.com>
2015-10-23 15:22:54 +0200

    no message

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=windows-1250">
+    <title>Titulek stránky</title>
+    <link rel="stylesheet" href="styly/muj-styl.css" type="text/css">
+</head>
+<body>
 <?php
 echo 'ahoj';
 ?>
+</body>
+</html>
